394c60c5bbea80a9927e9d0fdb91f8285e1d02cd
eddypv <eddypv0@gmail.com>
2018-08-13 17:01:11 -0400

    [UPDATE] incluir los botones de play y pausa

---
+++ src/player/components/play-pause.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+function PlayPause(props){
+    return (
+        <div>
+            {
+                props.pause ?
+                <button onClick={props.handleClick}>
+                    <Play size={25} color="white" />   
+                </button>
+                :
+                <button onClick={props.handleClick}>
+                    <Pause size={25} color="white" />   
+                </button>
+            }
+        </div>
+    )
+}
+
+export default PlayPause;
--- src/player/container/video-player.js
+++ src/player/container/video-player.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video';
-import Title from '../components/title'
+import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 class VideoPlayer extends Component{
-
+    state = {
+        pause:false
+    }
+    togglePlay =(event) =>{
+        this.setState({
+            pause:!this.state.pause
+        })
+    }
     render(){
         return (
             <VideoPlayerLayout>
                 <Title title="Ejemplo de it" />
+                <PlayPause 
+                    handleClick={this.togglePlay}
+                    pause={this.state.pause}    
+                />
                 <Video 
                     autoPlay={true}
                     src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
