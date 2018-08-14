11f2b2a2e3c93422f52f8bcab757bc9bce626e4f
eddypv <eddypv0@gmail.com>
2018-08-14 11:31:16 -0400

    [ADD] Incluir progress bar

--- src/player/container/video-player.js
+++ src/player/container/video-player.js
@@ -5,6 +5,7 @@ import Title from '../components/title';
 import PlayPause from '../components/play-pause';
 import Timer from '../components/timer'
 import Controls from '../components/video-player-controls';
+import ProgressBar from '../components/progress-bar'
 class VideoPlayer extends Component{
     state = {
         pause:false,
@@ -32,6 +33,11 @@ class VideoPlayer extends Component{
             currentTime:this.video.currentTime
         })
     }
+    handleProgressChange =(event)=>{
+        this.video.currentTime = event.target.value
+        
+        
+    }
     render(){
         return (
             <VideoPlayerLayout>
@@ -44,6 +50,11 @@ class VideoPlayer extends Component{
                     <Timer 
                         duration={this.state.duration}
                         currentTime={this.state.currentTime}
+                    />
+                    <ProgressBar 
+                        duration={this.state.duration}
+                        value={this.state.currentTime}
+                        handleProgressChange={this.handleProgressChange}
                     />
                 </Controls>
                 <Video 
