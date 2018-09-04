5ad43b8fd9ed6f9b5125d346ec91bdbd29e8e39c
eddypv <eddypv0@gmail.com>
2018-09-03 20:34:28 -0400

    [UPDATE] Redux con vanilla js

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -8,7 +8,12 @@ function handleSubmit(event){
     event.preventDefault();
     const data = new FormData($form);
     const title = data.get("title");
-    console.log(title);
+    store.dispatch({
+        type:"ADD_SONG",
+        payload:{
+            title:title
+        }
+    })
 }
 const initialState = [
     {
@@ -21,15 +26,33 @@ const initialState = [
         "title":"Echame la culpa"
     }
 ]
+const reducer = function(state, action){
+    switch(action.type){
+        case 'ADD_SONG':
+            return [...state,action.payload]
+            break;
+        default:
+            return state
+
+    }
+}
 const store = createStore(
-    (state)=>state,
+    reducer,
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-const $container = document.getElementById("playlist");
-const playlist = store.getState();
-playlist.forEach(function(item){
-    const template = document.createElement('p');
-    template.textContent = item.title;
-    $container.appendChild(template);
-})
+function render(){
+    const $container = document.getElementById("playlist");
+    $container.innerHTML="";
+    const playlist = store.getState();
+    playlist.forEach(function(item){
+        const template = document.createElement('p');
+        template.textContent = item.title;
+        $container.appendChild(template);
+    })
+}
+render();
+function handleChange(){
+    render();
+}
+store.subscribe(handleChange);
