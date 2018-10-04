3ec97fc06d7a52c67682c9e3c71bc9c9a150267b
eddypv <eddypv0@gmail.com>
2018-10-03 20:53:21 -0400

    [UPDATE] Crear un middleware

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,16 +3,34 @@ import ReactDOM from 'react-dom'
 //import Playlist from "./src/playlist/components/playlist"
 //import data from '../api.json'
 import Home from '../pages/container/home'
-import {createStore} from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
 import reducer from '../reducer/index';
 import {Map as map} from 'immutable';
 
-
+function looger({getState, dispatch}){
+    return (next)=>{
+        return (action)=>{
+            console.log("Mi viejo estado", getState().toJS())
+            console.log("vamos enviar nuestra accion", action)
+            const value = next(action)
+            console.log("Este es mi nuevo estado", getState().toJS())
+            return value
+        }
+    }
+}
+const loogerES6 = ({getState, dispatch})=> next => action=>{
+    console.log("Mi viejo estado", getState().toJS())
+    console.log("vamos enviar nuestra accion", action)
+    const value = next(action)
+    console.log("Este es mi nuevo estado", getState().toJS())
+    return value
+}
 const store = createStore(
     reducer,
     map(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(loogerES6)
 )
 const app = document.getElementById("home-container")
 
